d207f128d62972c9339b0c3d887f6f65d0620f44
Musobek Madrimov <66286036+musobekmadrimov@users.noreply.github.com>
2021-05-15 14:51:49 +0500

    JS script for showing and hiding password...

---
+++ showHidePassword.js
@@ -0,0 +1,13 @@
+function showHidePassword() {
+        var passwordInput = document.getElementById("password");
+        var eyeIcon = document.getElementById("passwordEye");
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            eyeIcon.classList.remove('fa-eye');
+            eyeIcon.classList.add('fa-eye-slash')
+        } else {
+            passwordInput.type = "password";
+            eyeIcon.classList.remove('fa-eye-slash');
+            eyeIcon.classList.add('fa-eye')
+        }
+    }
